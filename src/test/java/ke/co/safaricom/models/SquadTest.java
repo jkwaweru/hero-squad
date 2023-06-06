45b82b6a37fe102d2c98ec9f34249a9c13b54bee
amani <akai@safaricom.co.ke>
2023-06-06 19:42:29 +0300

    Added SquadTest class

---
+++ src/test/java/ke/co/safaricom/models/SquadTest.java
@@ -0,0 +1,30 @@
+package ke.co.safaricom.models;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SquadTest {
+
+    Squad testSquad = new Squad("Coders", "Write Code", 7);
+
+    @Test
+    @DisplayName(value = "SQUAD NAME")
+    void getSquad() {
+        assertEquals("Coders", testSquad.getSquad());
+    }
+
+    @Test
+    @DisplayName(value = "SQUAD CAUSE")
+    void getCause() {
+        assertEquals("Write Code", testSquad.getCause());
+    }
+
+    @Test
+    @DisplayName(value = "SQUAD SIZE")
+    void getSize() {
+        assertEquals(7, testSquad.getSize());
+    }
+
+}
